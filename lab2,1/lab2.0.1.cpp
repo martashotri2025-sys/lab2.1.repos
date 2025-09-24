4610d614ed1179edbd60323888c447eeacae516f
Марта <marta.shot.ri.2025@lpnu.ua>
2025-09-25 01:30:01 +0300

    Змін, яких немає у віддаленому: видалила змінну z3

--- lab2,1/lab2.0.1.cpp
+++ lab2,1/lab2.0.1.cpp
@@ -11,8 +11,7 @@ int main()
 	double y;
 	double z1;
 	//double z2;
-	double z3;
-
+	
 	cout << "x= ";
 	cin >> x;
 	cout << "y= ";
@@ -20,11 +19,11 @@ int main()
 
 	z1 = pow(cos(x), 4) + pow(sin(y), 2) + 0.25 * pow(sin(2 * x), 2) - 1;
 	//z2 = sin(y + x) * sin(y - x);
-	z3 = x+y;
+	
 
 	cout << "z1= " << z1 << endl;
     cout << "z2= " << z2 << endl;
-	cout << "z3= " << z3 << endl;
+	
 
 
 
